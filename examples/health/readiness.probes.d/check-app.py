579486652e26b9f0925f8d020dbd098ba82016ab
Pascal Zimmermann <pascal.zimmermann@theiotstudio.com>
2021-06-26 09:33:39 +0200

    Implement a new health service, update the examples and update the documentation

--- examples/health/readiness.probes.d/check-app.py
+++ examples/health/readiness.probes.d/check-app.py
@@ -1,9 +1,11 @@
+
 #!/usr/bin/python3
 
 import importlib.util
 import os
 import sys
-import subprocess
+
+import psycopg2
 
 spec = importlib.util.spec_from_file_location(
     "module.name", f"{os.environ.get('IMAGE_BASE_DIR')}{os.sep}utils.py"
@@ -14,23 +16,44 @@ spec.loader.exec_module(utils)
 result_local = utils.is_supervisor_process_running("app")
 
 if result_local == 0:
-    sys.stdout.write("Trying to call main page at localhost:27017/test;")
+    sys.stdout.write("Trying to call the database;")
 
-    command = ["echo", "db.runCommand('ping').ok", "|", "mongo", "localhost:27017/test"]
-    result = subprocess.run(
-        command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True
-    )
+    try:
+        sys.stdout.write("Create a PostgreSQL connection;")
+        conn = psycopg2.connect(
+            f"user={utils.get_env_variable('POSTGRES_INITDB_MONITORING_USERNAME')} "
+            f"password={utils.get_env_variable('POSTGRES_INITDB_MONITORING_PASSWORD')} "
+            f"host={utils.get_env_variable('POSTGRES_INITDB_HOST')} "
+            f"port={utils.get_env_variable('POSTGRES_INITDB_PORT')} "
+            f"dbname=postgres "
+            f"ca_cert={utils.get_env_variable('STORAGE_CONF_DIR')}{os.sep}ca.crt sslmode=verify-ca"
+        )
+    except Exception as e:
+        sys.stderr.write(f"Can not create the connection. Check the error: {e};")
+        sys.exit(1)
 
-    if result.returncode != 0:
-        sys.stderr.write("Main page didn't respond!;")
-        result_local = 1
-    else:
-        sys.stdout.write(f"Main page responded with HTTP Status {result.stdout}")
-        if 200 <= int(result.stdout) <= 400:
-            result_local = 0
+    try:
+        sys.stdout.write("Create a PostgreSQL cursor;")
+        cur = conn.cursor()
+    except Exception as e:
+        sys.stderr.write(f"Can not create the cursor. Check the error: {e};")
+        sys.exit(1)
+
+    cur.execute("SELECT * FROM postgres;")
+    result = cur.fetchone()
+
+    if result != "" or result is not None:
+        sys.stderr.write("Main command didn't respond!;")
+        if bool(utils.get_env_variable("IMAGE_HEALTH_LIVENESS_FORCE_REBOOT")):
+            response = utils.restart_process("app")
+
+            if response is not None:
+                result_local = 1
         else:
-            sys.stderr.write("Main page could not be called.;")
             result_local = 1
+    else:
+        sys.stdout.write(f"Main command responded with error state")
+        result_local = 0
 else:
     sys.stderr.write("App is not running!;")
 
---
+++ examples/health/readiness.probes.d/check-mongodb.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import importlib.util
+import os
+import sys
+import subprocess
+
+spec = importlib.util.spec_from_file_location("module.name", f"{os.environ.get('IMAGE_BASE_DIR')}{os.sep}utils.py")
+utils = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(utils)
+
+result_local = utils.is_supervisor_process_running("app")
+
+if result_local == 0:
+    sys.stdout.write("Trying to call main page at localhost:27017/test;")
+
+    command = ["echo", "db.runCommand('ping').ok", "|", "mongo", "localhost:27017/test"]
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+
+    if result.returncode != 0:
+        sys.stderr.write("Main page didn't respond!;")
+        if bool(utils.get_env_variable("IMAGE_HEALTH_LIVENESS_FORCE_REBOOT")):
+            response = utils.restart_process("app")
+
+            if response is not None:
+                result_local = 1
+        else:
+            result_local = 1
+    else:
+        sys.stdout.write(f"Main page responded with HTTP status {result.stdout}")
+        if 200 <= int(result.stdout) <= 400:
+            result_local = 0
+        else:
+            sys.stderr.write("Main page could not be called;")
+            if bool(utils.get_env_variable("IMAGE_HEALTH_LIVENESS_FORCE_REBOOT")):
+                response = utils.restart_process("app")
+
+                if response is not None:
+                    result_local = 1
+            else:
+                result_local = 1
+else:
+    sys.stderr.write("App is not running!;")
+
+sys.exit(result_local)
